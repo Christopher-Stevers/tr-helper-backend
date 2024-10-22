db71ad849912d5234f81dabdc147b2cb9f9e202e
Christopher-Stevers <christopher.stevers@protonmail.com>
2024-10-22 10:50:01 -0400

    allow corss sight

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
-app.use(cors());
+app.use(cors("https://tr-helper-next.vercel.app/"));
 
 // Express route to handle file processing
 
